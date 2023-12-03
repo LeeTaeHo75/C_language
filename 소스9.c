1508070c7c27e941a94f6fdee0a55b76cba282ca
LeeTaeHo75 <thlee48496@gachon.ac.kr>
2023-12-03 16:19:29 +0900

    두개의 point 구조체를 받아서 일치하면 1을 반환하고, 그렇지 않으면 0을 반환하는 함수 int equal(struct point p1, struct point p2)

---
+++ 소스9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct point {
+    int x;
+    int y;
+};
+
+int equal(struct point p1, struct point p2) {
+    return (p1.x == p2.x && p1.y == p2.y);
+}
+
+int main() {
+    struct point point1 = {1, 2};
+    struct point point2 = {1, 2};
+
+    if (equal(point1, point2))
+        printf("두 점은 일치합니다.\n");
+    else
+        printf("두 점은 일치하지 않습니다.\n");
+
+    return 0;
+}

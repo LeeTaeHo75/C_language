f2e5cd26d2f49feb760b543a94b1affa1f4258e3
LeeTaeHo75 <thlee48496@gachon.ac.kr>
2023-12-03 16:22:14 +0900

    git add .

---
+++ 소스10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void set_max_ptr(int arr[], int size, int **pmax) {
+    int max_index = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > arr[max_index]) {
+            max_index = i;
+        }
+    }
+    *pmax = &arr[max_index];
+}
+
+int main() {
+    int m[7] = {10, 30, 20, 50, 70, 90, 40};
+    int *pmax;
+
+    set_max_ptr(m, 7, &pmax);
+
+    printf("가장 큰 값: %d\n", *pmax);
+
+    return 0;
+}
+
